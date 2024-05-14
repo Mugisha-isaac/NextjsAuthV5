1ebe11c0ae07ea6d53a98dc59599ef02ad6cc607
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-05-14 13:51:43 +0200

    ft: add welcome page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,21 @@
 
+import { LoginButton } from "@/components/ui/login-button";
 import {Button} from  "../components/ui/button";
 
 export default function Home() {
-  return <Button>Log in with email</Button>
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-sky-500">
+     <div className="space-y-6">
+        <h1 className="text-6xl font-bold text-white drop-shadow-md">Auth</h1>
+        <p className="text-white text-lg">Auth.js authentication demo</p>
+        <div>
+          <LoginButton>
+            <Button size="lg" variant="secondary">
+             Sign in
+            </Button>
+          </LoginButton>
+        </div>
+     </div>
+    </main>
+  )
 }
