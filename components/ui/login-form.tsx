08680864b6fdabd9fe7353cf304552204bfd9770
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-05-14 15:40:05 +0200

    ft: add react useTransition hook

--- components/ui/login-form.tsx
+++ components/ui/login-form.tsx
@@ -1,81 +1,101 @@
-"use client"
+"use client";
 
-import {Form,FormControl,FormField,FormItem,FormLabel,FormMessage} from "./form";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card";
-import {useForm} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod";
-import * as z from 'zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./card";
+import { useForm } from "react-hook-form";
+import { useTransition } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
 import { LoginSchema } from "@/schemas";
 import { login } from "@/actions/login";
 import { Button } from "./button";
 import { Input } from "./input";
 import Link from "next/link";
 
-export const LogInForm = () =>{
-    const form = useForm<z.infer<typeof LoginSchema>>({
-        resolver: zodResolver(LoginSchema),
-        defaultValues: {
-            email: '',
-            password: ''
-        }
-    });
+export const LogInForm = () => {
 
-    const onSubmit = (values: z.infer<typeof LoginSchema>) =>{
-       login(values);
-    }
+    const [isPending,startTransition] = useTransition();
 
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    return (
-        <Card className="w-[400px]">
-            <CardHeader>
-                <CardTitle>Auth.js</CardTitle>
-                <CardDescription>Welcome!</CardDescription>
-            </CardHeader>
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    startTransition(async () => {
+        await login(values);
+      });
+  };
 
-            <CardContent>
+  return (
+    <Card className="w-[400px]">
+      <CardHeader>
+        <CardTitle>Auth.js</CardTitle>
+        <CardDescription>Welcome!</CardDescription>
+      </CardHeader>
 
-                <Form {...form}>
-                  <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-7">
-                   <div className="space-y-4">
-                      <FormField
-                        control={form.control}
-                        name="email"
-                        render={({field}) => (
-                            <FormItem>
-                                <FormLabel>Email</FormLabel>
-                                <FormControl>
-                                    <Input {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                      />
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-7">
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                      <FormField 
-                       control={form.control}
-                       name="password"
-                       render = {({field}) => (
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            <FormControl>
-                                <Input {...field} type="password" placeholder="******" />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                       )}
-                      />
-                   </div>
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="password" placeholder="******" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
 
-                   <Button type="submit" size='lg' className="w-full">Sign in</Button>
-                  </form>
-                </Form>
-            </CardContent>
+            <Button type="submit" size="lg" className="w-full" disabled={isPending}>
+              Sign in
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
 
-            <CardFooter className="flex justify-between flex-col">
-               <Link className="text-xs " href='/auth/register'>
-                Don't have an account
-               </Link>
-            </CardFooter>
-        </Card>
-    )
-}
+      <CardFooter className="flex justify-between flex-col">
+        <Link className="text-xs " href="/auth/register">
+          Don't have an account
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+};
