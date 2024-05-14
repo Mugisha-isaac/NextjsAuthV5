31c45bef2f8673a446d165347c8303408d0a4613
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-05-14 15:04:30 +0200

    ft: login form validation using zod

--- components/ui/login-form.tsx
+++ components/ui/login-form.tsx
@@ -1,5 +1,24 @@
+"use client"
 
+import {Form,FormControl,FormField,FormItem,FormLabel,FormMessage} from "./form";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./card";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import * as z from 'zod';
+import { LoginSchema } from "@/schemas";
+import { Button } from "./button";
+import { Input } from "./input";
+import Link from "next/link";
 
 export const LogInForm = () =>{
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: '',
+            password: ''
+        }
+    });
+
+    
     return <span>Login Page</span>
 }
