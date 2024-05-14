65def50e7b6698515cd86e851797c1602e21a855
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-05-14 15:21:55 +0200

    ft: login form ui with validations

--- components/ui/login-form.tsx
+++ components/ui/login-form.tsx
@@ -19,6 +19,58 @@ export const LogInForm = () =>{
         }
     });
 
-    
-    return <span>Login Page</span>
+
+    return (
+        <Card className="w-[400px]">
+            <CardHeader>
+                <CardTitle>Auth.js</CardTitle>
+                <CardDescription>Welcome!</CardDescription>
+            </CardHeader>
+
+            <CardContent>
+
+                <Form {...form}>
+                  <form onSubmit={form.handleSubmit(()=> {})} className="space-y-7">
+                   <div className="space-y-4">
+                      <FormField
+                        control={form.control}
+                        name="email"
+                        render={({field}) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                      />
+
+                      <FormField 
+                       control={form.control}
+                       name="password"
+                       render = {({field}) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input {...field} type="password" placeholder="******" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                       )}
+                      />
+                   </div>
+
+                   <Button type="submit" size='lg' className="w-full">Sign in</Button>
+                  </form>
+                </Form>
+            </CardContent>
+
+            <CardFooter className="flex justify-between flex-col">
+               <Link className="text-xs " href='/auth/register'>
+                Don't have an account
+               </Link>
+            </CardFooter>
+        </Card>
+    )
 }
