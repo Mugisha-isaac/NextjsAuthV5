93fe1b81140066c9f776984fbaccb03b80e3ba15
Mugisha Isaac <mugishaisaac0508@gmail.com>
2024-05-16 14:28:51 +0200

    ft: add prisma

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,39 @@
-"use server"
+"use server";
 
-export const register = async(values: any) =>{
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    console.log(values);
-}
+import prisma from "@/lib/db";
+import * as z from "zod";
+import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcryptjs";
+
+export const register = async (values: any) => {
+  const validatedFields = RegisterSchema.safeParse(values);
+  if (!validatedFields.success) {
+    return {
+      error: "Invalid fields",
+    };
+  }
+
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await prisma.user.findUnique({
+    where: { email: email },
+  });
+
+  if (existingUser) {
+    return {
+      error: "User already exists",
+    };
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return {
+    success: "User created successfully!",
+  };
+};
